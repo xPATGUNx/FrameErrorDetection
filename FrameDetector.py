13913b33a1af34d2146d54643415b11de68746ec
Pascal Rutkowski <P.Rutkowski@steinberg.de>
2021-01-13 15:14:32 +0100

    Implement frame counting

--- FrameDetector.py
+++ FrameDetector.py
@@ -2,15 +2,18 @@ import cv2 as cv
 import numpy as np
 from pyzbar.pyzbar import decode
 import time
+from collections import Counter
+import typing
 
 
-def qr_code_detection(video_file_path):
+def qr_code_detection(video_file_path: str):
     cap = cv.VideoCapture(video_file_path)
     timestr = time.strftime("%Y %m %d-%H%M%S")
     scan_data_name = ('QR Scan Results ' + timestr + '.txt')
     scan_data_dir = ('Scan Results/' + scan_data_name)
     scan_data = open(scan_data_dir, 'w')
     scan_data.write('Detected frames for file "' + video_file_path + '":\n')
+    frame_index_list = []
 
     if not cap.isOpened():
         print("Error opening video stream or file")
@@ -26,26 +29,38 @@ def qr_code_detection(video_file_path):
             text_data = str(qr_code_scanner(frame))
             if not text_data == 'None':
                 scan_data.write(text_data + '\n')
+                frame_index_list.append(int(text_data))
             else:
                 scan_data.write('QR-Code could not be found.\n')
         else:
             break
 
+    print(frame_index_list)
     end = time.time()
     scan_data.close()
     cap.release()
     cv.destroyAllWindows()
-    print('Scan completed after ' + str(end - start) + ' seconds.')
+    # print('Scan completed after ' + str(end - start) + ' seconds.')
     print('"' + scan_data_name + '"' + ' has been created.')
+    # ToDo: replace magic number
+    frame_counter(frame_index_list, 180)
 
 
 def qr_code_scanner(obj):
     qr_code = decode(obj)
     for code in qr_code:
         data = code.data.decode('utf-8')
-        print(data)
+        # print(data)
         return data
 
 
+def frame_counter(scan_list: list, amount_of_frames):
+    for current_frame in range(amount_of_frames + 1):
+        occurrance = scan_list.count(current_frame)
+        print('Frame ' + str(current_frame) + ' occurred ' + str(occurrance) + ' times.')
+
+
 if __name__ == '__main__':
-    qr_code_detection('Video/No_QR_Test.mp4')
+    # qr_code_detection('Video/No_QR_Test.mp4')
+    # qr_code_detection('Video/QRCodeScreenCap.mp4')
+    qr_code_detection('Video/Mac Book Pro Capture.mp4')
