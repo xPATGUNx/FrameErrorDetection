d34399a5f0238fdf7372897d760adbda62a3e965
Pascal Rutkowski <P.Rutkowski@steinberg.de>
2021-01-14 13:00:56 +0100

    Refactor test-file-data creation

--- FrameDetector.py
+++ FrameDetector.py
@@ -9,19 +9,12 @@ import os
 
 def qr_code_detection(video_file_name: str, expected_amount_of_frames):
     cap = cv.VideoCapture('Video/' + video_file_name)
-    name_without_suffix = os.path.splitext(video_file_name)[0]
-    timestr = time.strftime("%Y %m %d-%H%M%S")
-    scan_data_name = (str(name_without_suffix) + ' Scan Results ' + timestr + '.txt')
-    scan_data_dir = ('Scan Results/' + scan_data_name)
-    scan_data = open(scan_data_dir, 'w')
-    scan_data.write('Detected frames for file "' + video_file_name + '":\n')
     frame_index_list = []
 
     if not cap.isOpened():
         print("Error opening video stream or file")
 
     print('Scanning for frame IDs...')
-    time.sleep(2)
     start = time.time()
     while cap.isOpened():
 
@@ -30,21 +23,18 @@ def qr_code_detection(video_file_name: str, expected_amount_of_frames):
         if ret:
             text_data = str(qr_code_scanner(frame))
             if not text_data == 'None':
-                scan_data.write(text_data + '\n')
                 frame_index_list.append(int(text_data))
             else:
-                scan_data.write('QR-Code could not be found.\n')
+                frame_index_list.append('QR code was not readable.')
         else:
             break
 
     print(frame_index_list)
     end = time.time()
-    scan_data.close()
     cap.release()
     cv.destroyAllWindows()
     # print('Scan completed after ' + str(end - start) + ' seconds.')
-    print('"' + scan_data_name + '"' + ' has been created.')
-    frame_counter(frame_index_list, expected_amount_of_frames)
+    test_data_file_writer(frame_index_list, expected_amount_of_frames, video_file_name)
 
 
 def qr_code_scanner(obj):
@@ -55,13 +45,26 @@ def qr_code_scanner(obj):
         return data
 
 
-def frame_counter(scan_list: list, amount_of_frames):
+def test_data_file_writer(scan_list: list, amount_of_frames, file_name):
+    name_without_suffix = os.path.splitext(file_name)[0]
+    timestr = time.strftime("%Y %m %d-%H%M%S")
+    scan_data_name = (str(name_without_suffix) + ' Scan Results ' + timestr + '.txt')
+    scan_data_dir = ('Scan Results/' + scan_data_name)
+    scan_data = open(scan_data_dir, 'w')
+    scan_data.write('Detected frames for file "' + file_name + '":\n')
+
     for current_frame in range(amount_of_frames + 1):
         occurrance = scan_list.count(current_frame)
-        print('Frame ' + str(current_frame) + ' occurred ' + str(occurrance) + ' times.')
+        scan_data.write('Frame ' + str(current_frame) + ' occurred ' + str(occurrance) + ' times.\n')
+    not_readable_frames = scan_list.count('QR code was not readable.')
+    scan_data.write('QR code was not readable for ' + str(not_readable_frames) + ' frames.')
+    scan_data.close()
+    print('"' + scan_data_name + '"' + ' has been created.')
 
 
 if __name__ == '__main__':
     # qr_code_detection('No_QR_Test.mp4', 180)
     # qr_code_detection('QRCodeScreenCap.mp4', 180)
-    qr_code_detection('Mac Book Pro Capture.mp4', 180)
+    qr_code_detection('MacBook Pro QuickTime Capture.mp4', 180)
+    qr_code_detection('MacBook Pro Cubase  Capture 01.mp4', 180)
+    qr_code_detection('MacBook Pro Cubase  Capture 02.mp4', 180)
