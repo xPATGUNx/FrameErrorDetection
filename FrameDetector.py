c152c8f930bce1e8358eee1cf6db5369f7152469
Pascal Rutkowski <P.Rutkowski@steinberg.de>
2021-01-14 11:11:11 +0100

    Refactor file naming

--- FrameDetector.py
+++ FrameDetector.py
@@ -4,15 +4,17 @@ from pyzbar.pyzbar import decode
 import time
 from collections import Counter
 import typing
+import os
 
 
-def qr_code_detection(video_file_path: str, expected_amount_of_frames):
-    cap = cv.VideoCapture(video_file_path)
+def qr_code_detection(video_file_name: str, expected_amount_of_frames):
+    cap = cv.VideoCapture('Video/' + video_file_name)
+    name_without_suffix = os.path.splitext(video_file_name)[0]
     timestr = time.strftime("%Y %m %d-%H%M%S")
-    scan_data_name = ('QR Scan Results ' + timestr + '.txt')
+    scan_data_name = (str(name_without_suffix) + ' Scan Results ' + timestr + '.txt')
     scan_data_dir = ('Scan Results/' + scan_data_name)
     scan_data = open(scan_data_dir, 'w')
-    scan_data.write('Detected frames for file "' + video_file_path + '":\n')
+    scan_data.write('Detected frames for file "' + video_file_name + '":\n')
     frame_index_list = []
 
     if not cap.isOpened():
@@ -42,7 +44,6 @@ def qr_code_detection(video_file_path: str, expected_amount_of_frames):
     cv.destroyAllWindows()
     # print('Scan completed after ' + str(end - start) + ' seconds.')
     print('"' + scan_data_name + '"' + ' has been created.')
-    # ToDo: replace magic number
     frame_counter(frame_index_list, expected_amount_of_frames)
 
 
@@ -61,6 +62,6 @@ def frame_counter(scan_list: list, amount_of_frames):
 
 
 if __name__ == '__main__':
-    # qr_code_detection('Video/No_QR_Test.mp4', 180)
-    # qr_code_detection('Video/QRCodeScreenCap.mp4', 180)
-    qr_code_detection('Video/Mac Book Pro Capture.mp4', 180)
+    # qr_code_detection('No_QR_Test.mp4', 180)
+    # qr_code_detection('QRCodeScreenCap.mp4', 180)
+    qr_code_detection('Mac Book Pro Capture.mp4', 180)
--- TestVideoRenderer.py
+++ TestVideoRenderer.py
@@ -81,4 +81,4 @@ def delete_temp_images():
 if __name__ == '__main__':
     render_test_video(video_file_path='Video/TestVideo.mp4', new_video_file_name='Video/TestVideoNew.mp4',
                       codec=cv.VideoWriter_fourcc(*'mp4v'))
-    # render_test_video(video_file_path='Video/TestVideo.mp4', new_video_file_name='Video/TestVideoNewAuto.mp4')
+    # render_test_video(video_file_name='Video/TestVideo.mp4', new_video_file_name='Video/TestVideoNewAuto.mp4')
