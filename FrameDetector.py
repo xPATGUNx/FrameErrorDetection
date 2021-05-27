8ba92a794f1d36fb019976f5ca7ace464940e59e
Pascal Rutkowski <P.Rutkowski@steinberg.de>
2021-05-27 13:23:42 +0200

    Restructure class to use class variables

--- FrameDetector.py
+++ FrameDetector.py
@@ -6,13 +6,17 @@ from pyzbar.pyzbar import decode
 
 
 class FrameDetector:
-    def __qr_code_detection(self, video_file_path: str):
+    def __init__(self):
+        self.video_file_path = ''
+        self.expected_amount_of_frames = 0
+        self.scan_list = []
+
+    def __qr_code_detection(self):
         """
         Iterates over every video frame individually and checks its QR code for frame index.
-        :param video_file_path: A string of the path to a video that needs to be analysed.
         :return: A list of every frame index.
         """
-        cap = cv.VideoCapture(video_file_path)
+        cap = cv.VideoCapture(self.video_file_path)
         frame_index_list = []
 
         if not cap.isOpened():
@@ -34,7 +38,7 @@ class FrameDetector:
         cap.release()
         cv.destroyAllWindows()
         print('Scan completed after ' + str(end - start) + ' seconds.')
-        return frame_index_list
+        self.scan_list = frame_index_list
 
     @staticmethod
     def __qr_code_scanner(frame):
@@ -49,95 +53,54 @@ class FrameDetector:
             # print(data)
             return data
 
-    @staticmethod
-    def __test_data_file_writer(scan_list: list, video_file_path: str, expected_amount_of_frames: int):
+    def __test_data_file_writer(self):
         """
         Generates a text file and fills it with data from the passed list.
-        :param scan_list: list of scanned frames
-        :param video_file_path: A string of the path to a video that needs to be analysed.
-        :param expected_amount_of_frames: An integer with the exact amount of expected video frames to check against.
         """
-        base_name = os.path.basename(video_file_path)
+        base_name = os.path.basename(self.video_file_path)
         timestr = time.strftime("%Y %m %d-%H%M%S")
         scan_data_name = ('Scan Results ' + timestr + str(base_name) + '.txt')
         scan_data_dir = ('Scan Results/' + scan_data_name)
         scan_data = open(scan_data_dir, 'w')
-        scan_data.write('Detected frames for file "' + video_file_path + '":\n')
+        scan_data.write('Detected frames for file "' + self.video_file_path + '":\n')
 
-        for current_frame in range(1, expected_amount_of_frames + 1):
-            occurrence = scan_list.count(current_frame)
+        for current_frame in range(1, self.expected_amount_of_frames + 1):
+            occurrence = self.scan_list.count(current_frame)
             scan_data.write('Frame ' + str(current_frame) + ' occurred ' + str(occurrence) + ' times.\n')
-        not_readable_frames = scan_list.count('QR code was not readable.')
+        not_readable_frames = self.scan_list.count('QR code was not readable.')
         scan_data.write('QR code was not readable for ' + str(not_readable_frames) + ' frames.')
         scan_data.close()
         print('"' + scan_data_name + '"' + ' has been created.')
 
-    @staticmethod
-    def __list_video_frame_errors(scan_list: list, expected_amount_of_frames: int):
+    def __list_video_frame_errors(self):
         """
         Every frame has to occure only once. Every anomaly gets listed by this function.
-        :param scan_list: list of scanned frames
-        :param expected_amount_of_frames: An integer with the exact amount of expected video frames to check against.
         :return: Returns a list with all dropped or duplicated video frames.
         """
         list_of_problematic_frames = []
-        for current_frame in range(1, expected_amount_of_frames + 1):
-            occurrence = scan_list.count(current_frame)
+        for current_frame in range(1, self.expected_amount_of_frames + 1):
+            occurrence = self.scan_list.count(current_frame)
             if occurrence != 1:
                 list_of_problematic_frames.append('Frame ' + str(current_frame) +
                                                   ' occurred ' + str(occurrence) + ' times.')
         return list_of_problematic_frames
 
-    def frame_drop_detection(self, video_file_path: str, expected_amount_of_frames: int):
+    def set_parameters(self, video_file_path: str, expected_amount_of_frames: int):
+        """
+        Sets all parameters needed to use frame drop detection on a desired video.
+        :param video_file_path: A string of the video_file_path to a video that needs to be analysed.
+        :param expected_amount_of_frames: An integer with the exact amount of expected video frames to check against.
+        :return:
+        """
+        self.video_file_path = video_file_path
+        self.expected_amount_of_frames = expected_amount_of_frames
+
+    def frame_drop_detection(self):
         """
         Core function to detect frame drops & frame duplicates in a video file.
-        :param video_file_path: A string of the path to a video that needs to be analysed.
-        :param expected_amount_of_frames: An integer with the exact amount of expected video frames to check against.
         :return: Returns a list with all dropped or duplicated video frames.
         """
-        video_frames = self.__qr_code_detection(video_file_path)
-        self.__test_data_file_writer(video_frames, video_file_path, expected_amount_of_frames)
-        list_of_detected_frame_drops = self.__list_video_frame_errors(video_frames, expected_amount_of_frames)
+        self.__qr_code_detection()
+        self.__test_data_file_writer()
+        list_of_detected_frame_drops = self.__list_video_frame_errors()
         return list_of_detected_frame_drops
-
-
-if __name__ == '__main__':
-    # frame_drop_detection('No_QR_Test.mp4', 180)
-    # frame_drop_detection('QRCodeScreenCap.mp4', 180)
-    # frame_drop_detection('MacBook Pro QuickTime Capture.mp4', 180)
-    # frame_drop_detection('MacBook Pro Cubase  Capture 01.mp4', 180)
-    # frame_drop_detection('MacBook Pro Cubase  Capture 02.mp4', 180)
-    # frame_drop_detection('MacBook Pro QuickTime 60 FPS.mp4', 150)
-
-    # frame_drop_detection('Intensity Pro 4K Captures/MacBook Pro 60 FPS Cubase 60 FPS Capture 1.mp4', 150)
-    # frame_drop_detection('Intensity Pro 4K Captures/MacBook Pro 60 FPS Cubase 60 FPS Capture 2.mp4', 150)
-    # frame_drop_detection('Intensity Pro 4K Captures/MacBook Pro 59.97 FPS Cubase 60 FPS Capture 1.mp4', 150)
-    # frame_drop_detection('Intensity Pro 4K Captures/MacBook Pro 50 FPS Cubase 60 FPS Capture 1.mp4', 150)
-    # frame_drop_detection('Intensity Pro 4K Captures/MacBook Pro 30 FPS Cubase 60 FPS Capture 1.mp4', 150)
-    # frame_drop_detection('Intensity Pro 4K Captures/MacBook Pro 29.97 FPS Cubase 60 FPS Capture 1.mp4', 150)
-    # frame_drop_detection('Intensity Pro 4K Captures/MacBook Pro 25 FPS Cubase 60 FPS Capture 1.mp4', 150)
-    # frame_drop_detection('Intensity Pro 4K Captures/MacBook Pro 24 FPS Cubase 60 FPS Capture 1.mp4', 150)
-
-    # frame_drop_detection('LG4K/LG4K 30FPS Video 60FPS Capture.mp4', 150)
-    # frame_drop_detection('LG4K/LG4K 60FPS Video 60FPS Capture.mp4', 150)
-    # frame_drop_detection('LG4K/LG4K 60 FPS Video 120 FPS Capture.mp4', 150)
-    # frame_drop_detection('LG4K/LG4K 60FPS VLC 120FPS OBS.mp4', 150)
-    # frame_drop_detection('LG4K/LG4K 60FPS VLC 60FPS OBS.mp4', 150)
-    # frame_drop_detection('LG4K/LG4K 59FPS VLC 59FPS OBS.mp4', 150)
-    # frame_drop_detection('LG4K/LG4K 59FPS VLC 59FPS OBS V2.mp4', 150)
-    # frame_drop_detection('LG4K/LG4K 50FPS VLC 50FPS OBS.mp4', 150)
-    # frame_drop_detection('LG4K/LG4K 30FPS VLC 30FPS OBS.mp4', 150)
-    # frame_drop_detection('LG4K/LG4K 29FPS VLC 29FPS OBS.mp4', 150)
-    # frame_drop_detection('LG4K/LG4K 25FPS VLC 25FPS OBS.mp4', 150)
-
-    # frame_drop_detection('Elgato/24 FPS.mp4', 150)
-    # frame_drop_detection('Elgato/24 FPS new.mp4', 150)
-    # frame_drop_detection('Elgato/25 FPS.mp4', 150)
-    # frame_drop_detection('Elgato/29 FPS.mp4', 150)
-    # frame_drop_detection('Elgato/29 fps new.mp4', 150)
-    # frame_drop_detection('Elgato/30 FPS.mp4', 150)
-    # frame_drop_detection('Elgato/59 FPS.mp4', 150)
-    # frame_drop_detection('Elgato/50 FPS.mp4', 150)
-    # frame_drop_detection('Elgato/60 FPS.mp4', 150)
-    frame_detector = FrameDetector()
-    frame_detector.frame_drop_detection('Video/Elgato/60 FPS.mp4', 150)
--- TestForFrameDrops.py
+++ TestForFrameDrops.py
@@ -7,12 +7,14 @@ class TestFrameDropDetection(unittest.TestCase):
         video = 'Elgato/60 FPS.mp4'
         expected_video_frames = 150
         frame_detector = FrameDetector()
-        detected_frame_drops = frame_detector.frame_drop_detection(video, expected_video_frames)
+        frame_detector.set_parameters(video, expected_video_frames)
+        detected_frame_drops = frame_detector.frame_drop_detection()
         self.assertEqual(len(detected_frame_drops), 0, "A problem has been detected: " + str(detected_frame_drops))
 
     def test_mkv_video_for_frame_drops(self):
         video = 'Video/Elgato/60 FPS.mkv'
         expected_video_frames = 150
         frame_detector = FrameDetector()
-        detected_frame_drops = frame_detector.frame_drop_detection(video, expected_video_frames)
+        frame_detector.set_parameters(video, expected_video_frames)
+        detected_frame_drops = frame_detector.frame_drop_detection()
         self.assertEqual(len(detected_frame_drops), 0, "A problem has been detected: " + str(detected_frame_drops))
