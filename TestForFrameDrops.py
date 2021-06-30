6b92dc06d5f808c132a9ccd3b00daf658ae6e712
Pascal Rutkowski <P.Rutkowski@steinberg.de>
2021-06-30 14:08:03 +0200

    Add Video capture test routine

--- TestForFrameDrops.py
+++ TestForFrameDrops.py
@@ -76,9 +76,17 @@ class TestFrameDropDetection(unittest.TestCase):
         self.assertNotEqual(len(detected_frame_drops), 0, "A problem has been detected: " + str(detected_frame_drops))
 
     def test_2h_video_for_frame_drops(self):
-        video = 'Video/QR_Code_Frame_180587FPS.mp4'
+        video = 'Video/QR_Code_Frame_180587Frames_25FPS.mp4'
         expected_video_frames = 180586
         frame_detector = FrameDetector()
         frame_detector.set_video_analysis_parameters(video, expected_video_frames)
         detected_frame_drops = frame_detector.frame_drop_detection()
+        self.assertEqual(len(detected_frame_drops), 0, "A problem has been detected: " + str(detected_frame_drops))
+
+    def test_capture_for_frame_drops(self):
+        video = 'Video/Elgato 30 FPS Lenovo 3.mkv'
+        expected_video_frames = 150
+        frame_detector = FrameDetector()
+        frame_detector.set_video_analysis_parameters(video, expected_video_frames)
+        detected_frame_drops = frame_detector.frame_drop_detection(crop_video=False)
         self.assertEqual(len(detected_frame_drops), 0, "A problem has been detected: " + str(detected_frame_drops))
