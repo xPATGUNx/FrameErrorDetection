bd726ce09e346f0a1cc5d9f3409ea2ecc81d9bb0
Pascal Rutkowski <P.Rutkowski@steinberg.de>
2021-07-29 09:57:02 +0200

    Implement new quality metrics features

--- FrameDetector.py
+++ FrameDetector.py
@@ -113,27 +113,47 @@ class FrameDetector:
         test_data_file_writer(video_file_path=self.video_file_path,
                               expected_amount_of_frames=self.expected_amount_of_frames,
                               scan_list=self.scan_list)
-
         if frames_per_second is None or frames_per_second == 60:
             list_of_detected_frame_drops = self.__list_video_frame_errors_default()
+            quality_metrics_report_writer(video_file_path=self.video_file_path,
+                                          expected_amount_of_frames=self.expected_amount_of_frames,
+                                          scan_list=self.scan_list, frame_error_dict=list_of_detected_frame_drops)
             return list_of_detected_frame_drops
         elif frames_per_second == 59.94:
             list_of_detected_frame_drops = self.__list_video_frame_errors_59_fps()
+            quality_metrics_report_writer(video_file_path=self.video_file_path,
+                                          expected_amount_of_frames=self.expected_amount_of_frames,
+                                          scan_list=self.scan_list, frame_error_dict=list_of_detected_frame_drops)
             return list_of_detected_frame_drops
         elif frames_per_second == 50:
             list_of_detected_frame_drops = self.__list_video_frame_errors_50_fps()
+            quality_metrics_report_writer(video_file_path=self.video_file_path,
+                                          expected_amount_of_frames=self.expected_amount_of_frames,
+                                          scan_list=self.scan_list, frame_error_dict=list_of_detected_frame_drops)
             return list_of_detected_frame_drops
         elif frames_per_second == 30:
             list_of_detected_frame_drops = self.__list_video_frame_errors_30_fps()
+            quality_metrics_report_writer(video_file_path=self.video_file_path,
+                                          expected_amount_of_frames=self.expected_amount_of_frames,
+                                          scan_list=self.scan_list, frame_error_dict=list_of_detected_frame_drops)
             return list_of_detected_frame_drops
         elif frames_per_second == 29.97:
             list_of_detected_frame_drops = self.__list_video_frame_errors_29_fps()
+            quality_metrics_report_writer(video_file_path=self.video_file_path,
+                                          expected_amount_of_frames=self.expected_amount_of_frames,
+                                          scan_list=self.scan_list, frame_error_dict=list_of_detected_frame_drops)
             return list_of_detected_frame_drops
         elif frames_per_second == 25:
             list_of_detected_frame_drops = self.__list_video_frame_errors_25_fps()
+            quality_metrics_report_writer(video_file_path=self.video_file_path,
+                                          expected_amount_of_frames=self.expected_amount_of_frames,
+                                          scan_list=self.scan_list, frame_error_dict=list_of_detected_frame_drops)
             return list_of_detected_frame_drops
         elif frames_per_second == 24:
             list_of_detected_frame_drops = self.__list_video_frame_errors_24_fps()
+            quality_metrics_report_writer(video_file_path=self.video_file_path,
+                                          expected_amount_of_frames=self.expected_amount_of_frames,
+                                          scan_list=self.scan_list, frame_error_dict=list_of_detected_frame_drops)
             return list_of_detected_frame_drops
         else:
             raise Exception(str(frames_per_second) + ' is a not supported framerate or a typo.')
@@ -155,34 +175,32 @@ class FrameDetector:
         Every frame has to occure only once. Every anomaly gets listed by this function.
         :return: Returns a list with all dropped or duplicated video frames.
         """
-        list_of_problematic_frames = []
+        dictionary_of_frame_errors = {}
         for current_frame in range(1, self.expected_amount_of_frames + 1):
             occurrence_of_current_frame = self.scan_list.count(current_frame)
             if occurrence_of_current_frame != 1:
-                list_of_problematic_frames.append('Frame ' + str(current_frame) +
-                                                  ' occurred ' + str(occurrence_of_current_frame) + ' times.')
-        return list_of_problematic_frames
+                dictionary_of_frame_errors[current_frame] = occurrence_of_current_frame
+        return dictionary_of_frame_errors
 
     def __list_video_frame_errors_59_fps(self):
         """
                 Lists every frame error that happens during recording of video playback in 59.94 fps.
                 :return: Returns a list with all dropped or duplicated video frames.
                 """
-        list_of_problematic_frames = []
+        dictionary_of_frame_errors = {}
         for current_frame in range(1, self.expected_amount_of_frames + 1):
             occurrence_of_current_frame = self.scan_list.count(current_frame)
             if occurrence_of_current_frame != 1 and occurrence_of_current_frame != 2:
-                list_of_problematic_frames.append('Frame ' + str(current_frame) +
-                                                  ' occurred ' + str(occurrence_of_current_frame) + ' times.')
+                dictionary_of_frame_errors[current_frame] = occurrence_of_current_frame
 
-        return list_of_problematic_frames
+        return dictionary_of_frame_errors
 
     def __list_video_frame_errors_50_fps(self):
         """
         Lists every frame error that happens during recording of video playback in 50 fps.
         :return: Returns a list with all dropped or duplicated video frames.
         """
-        list_of_problematic_frames = []
+        dictionary_of_frame_errors = {}
         for current_frame in range(1, self.expected_amount_of_frames + 1):
             occurrence_of_current_frame = self.scan_list.count(current_frame)
             occurrence_of_previous_frame = self.scan_list.count(current_frame - 1)
@@ -192,47 +210,43 @@ class FrameDetector:
                 if occurrence_of_current_frame == 2:
                     if occurrence_of_previous_frame != 1 and occurrence_of_next_frame != 1 \
                             and current_frame != self.expected_amount_of_frames and current_frame - 1 != 0:
-                        list_of_problematic_frames.append('Frame ' + str(current_frame) +
-                                                          ' occurred ' + str(occurrence_of_current_frame) + ' times.')
+                        dictionary_of_frame_errors[current_frame] = occurrence_of_current_frame
                 else:
-                    list_of_problematic_frames.append('Frame ' + str(current_frame) +
-                                                      ' occurred ' + str(occurrence_of_current_frame) + ' times.')
-        return list_of_problematic_frames
+                    dictionary_of_frame_errors[current_frame] = occurrence_of_current_frame
+        return dictionary_of_frame_errors
 
     def __list_video_frame_errors_30_fps(self):
         """
         Lists every frame error that happens during recording of video playback in 30 fps.
         :return: Returns a list with all dropped or duplicated video frames.
         """
-        list_of_problematic_frames = []
+        dictionary_of_frame_errors = {}
         for current_frame in range(1, self.expected_amount_of_frames + 1):
             occurrence_of_current_frame = self.scan_list.count(current_frame)
             if occurrence_of_current_frame != 2:
-                list_of_problematic_frames.append('Frame ' + str(current_frame) +
-                                                  ' occurred ' + str(occurrence_of_current_frame) + ' times.')
-        return list_of_problematic_frames
+                dictionary_of_frame_errors[current_frame] = occurrence_of_current_frame
+        return dictionary_of_frame_errors
 
     def __list_video_frame_errors_29_fps(self):
         """
         Lists every frame error that happens during recording of video playback in 29.97 fps.
         :return: Returns a list with all dropped or duplicated video frames.
         """
-        list_of_problematic_frames = []
+        dictionary_of_frame_errors = {}
         for current_frame in range(1, self.expected_amount_of_frames + 1):
             occurrence_of_current_frame = self.scan_list.count(current_frame)
             if occurrence_of_current_frame != 1 and occurrence_of_current_frame != 2 \
                     and occurrence_of_current_frame != 3:
-                list_of_problematic_frames.append('Frame ' + str(current_frame) +
-                                                  ' occurred ' + str(occurrence_of_current_frame) + ' times.')
+                dictionary_of_frame_errors[current_frame] = occurrence_of_current_frame
 
-        return list_of_problematic_frames
+        return dictionary_of_frame_errors
 
     def __list_video_frame_errors_25_fps(self):
         """
         Lists every frame error that happens during recording of video playback in 25 fps.
         :return: Returns a list with all dropped or duplicated video frames.
         """
-        list_of_problematic_frames = []
+        dictionary_of_frame_errors = {}
         for current_frame in range(1, self.expected_amount_of_frames + 1):
             occurrence_of_current_frame = self.scan_list.count(current_frame)
             occurrence_of_previous_frame = self.scan_list.count(current_frame - 1)
@@ -242,19 +256,17 @@ class FrameDetector:
                 if occurrence_of_current_frame == 3:
                     if occurrence_of_previous_frame != 2 and occurrence_of_next_frame != 2 \
                             and current_frame != self.expected_amount_of_frames and current_frame - 1 != 0:
-                        list_of_problematic_frames.append('Frame ' + str(current_frame) +
-                                                          ' occurred ' + str(occurrence_of_current_frame) + ' times.')
+                        dictionary_of_frame_errors[current_frame] = occurrence_of_current_frame
                 else:
-                    list_of_problematic_frames.append('Frame ' + str(current_frame) +
-                                                      ' occurred ' + str(occurrence_of_current_frame) + ' times.')
-        return list_of_problematic_frames
+                    dictionary_of_frame_errors[current_frame] = occurrence_of_current_frame
+        return dictionary_of_frame_errors
 
     def __list_video_frame_errors_24_fps(self):
         """
         Lists every frame error that happens during recording of video playback in 24 fps.
         :return: Returns a list with all dropped or duplicated video frames.
         """
-        list_of_problematic_frames = []
+        dictionary_of_frame_errors = {}
         for current_frame in range(1, self.expected_amount_of_frames + 1):
             occurrence_of_current_frame = self.scan_list.count(current_frame)
             occurrence_of_previous_frame = self.scan_list.count(current_frame - 1)
@@ -264,12 +276,10 @@ class FrameDetector:
                 if occurrence_of_current_frame == 3:
                     if occurrence_of_previous_frame != (2 or 3) and occurrence_of_next_frame != (2 or 3) \
                             and current_frame != self.expected_amount_of_frames and current_frame - 1 != 0:
-                        list_of_problematic_frames.append('Frame ' + str(current_frame) +
-                                                          ' occurred ' + str(occurrence_of_current_frame) + ' times.')
+                        dictionary_of_frame_errors[current_frame] = occurrence_of_current_frame
                 else:
-                    list_of_problematic_frames.append('Frame ' + str(current_frame) +
-                                                      ' occurred ' + str(occurrence_of_current_frame) + ' times.')
-        return list_of_problematic_frames
+                    dictionary_of_frame_errors[current_frame] = occurrence_of_current_frame
+        return dictionary_of_frame_errors
 
 
 """
