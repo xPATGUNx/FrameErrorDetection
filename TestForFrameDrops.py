a74e59664f73203127a7a132dc1f5f0e9e8c41e7
Pascal Rutkowski <P.Rutkowski@steinberg.de>
2021-08-17 11:29:02 +0200

    Refactor path name to qr videos

--- TestForFrameDrops.py
+++ TestForFrameDrops.py
@@ -4,7 +4,7 @@ from VideoScanner import VideoScanner
 
 class TestFrameDropDetection(unittest.TestCase):
     def test_60FPS_video_for_frame_drops(self):
-        video = 'Video/QR Code Videos/QR_150_Frames_60FPS.mp4'
+        video = 'Video/QR_Code_Videos/QR_150_Frames_60FPS.mp4'
         expected_video_frames = 150
         frame_detector = VideoScanner()
         frame_detector.set_video_analysis_parameters(video, expected_video_frames)
@@ -12,7 +12,7 @@ class TestFrameDropDetection(unittest.TestCase):
         self.assertEqual(len(detected_frame_drops), 0, "A problem has been detected: " + str(detected_frame_drops))
 
     def test_59FPS_video_for_frame_drops(self):
-        video = 'Video/QR Code Videos/QR_150_Frames_59.97FPS.mp4'
+        video = 'Video/QR_Code_Videos/QR_150_Frames_59.97FPS.mp4'
         expected_video_frames = 150
         frame_detector = VideoScanner()
         frame_detector.set_video_analysis_parameters(video, expected_video_frames)
@@ -20,7 +20,7 @@ class TestFrameDropDetection(unittest.TestCase):
         self.assertEqual(len(detected_frame_drops), 0, "A problem has been detected: " + str(detected_frame_drops))
 
     def test_50FPS_video_for_frame_drops(self):
-        video = 'Video/QR Code Videos/QR_150_Frames_50FPS.mp4'
+        video = 'Video/QR_Code_Videos/QR_150_Frames_50FPS.mp4'
         expected_video_frames = 150
         frame_detector = VideoScanner()
         frame_detector.set_video_analysis_parameters(video, expected_video_frames)
@@ -28,7 +28,7 @@ class TestFrameDropDetection(unittest.TestCase):
         self.assertEqual(len(detected_frame_drops), 0, "A problem has been detected: " + str(detected_frame_drops))
 
     def test_30FPS_video_for_frame_drops(self):
-        video = 'Video/QR Code Videos/QR_150_Frames_30FPS.mp4'
+        video = 'Video/QR_Code_Videos/QR_150_Frames_30FPS.mp4'
         expected_video_frames = 150
         frame_detector = VideoScanner()
         frame_detector.set_video_analysis_parameters(video, expected_video_frames)
@@ -36,7 +36,7 @@ class TestFrameDropDetection(unittest.TestCase):
         self.assertEqual(len(detected_frame_drops), 0, "A problem has been detected: " + str(detected_frame_drops))
 
     def test_29FPS_video_for_frame_drops(self):
-        video = 'Video/QR Code Videos/QR_150_Frames_29.97FPS.mp4'
+        video = 'Video/QR_Code_Videos/QR_150_Frames_29.97FPS.mp4'
         expected_video_frames = 150
         frame_detector = VideoScanner()
         frame_detector.set_video_analysis_parameters(video, expected_video_frames)
@@ -44,7 +44,7 @@ class TestFrameDropDetection(unittest.TestCase):
         self.assertEqual(len(detected_frame_drops), 0, "A problem has been detected: " + str(detected_frame_drops))
 
     def test_25FPS_video_for_frame_drops(self):
-        video = 'Video/QR Code Videos/QR_150_Frames_25FPS.mp4'
+        video = 'Video/QR_Code_Videos/QR_150_Frames_25FPS.mp4'
         expected_video_frames = 150
         frame_detector = VideoScanner()
         frame_detector.set_video_analysis_parameters(video, expected_video_frames)
@@ -52,7 +52,7 @@ class TestFrameDropDetection(unittest.TestCase):
         self.assertEqual(len(detected_frame_drops), 0, "A problem has been detected: " + str(detected_frame_drops))
 
     def test_24FPS_video_for_frame_drops(self):
-        video = 'Video/QR Code Videos/QR_150_Frames_24FPS.mp4'
+        video = 'Video/QR_Code_Videos/QR_150_Frames_24FPS.mp4'
         expected_video_frames = 150
         frame_detector = VideoScanner()
         frame_detector.set_video_analysis_parameters(video, expected_video_frames)
