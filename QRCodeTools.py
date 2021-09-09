50bb2e5b86cee01c6d1edff769fde5e7e10fb2bb
Pascal Rutkowski <P.Rutkowski@steinberg.de>
2021-09-09 12:14:10 +0200

    Enable of frame index printing

--- QRCodeTools.py
+++ QRCodeTools.py
@@ -12,7 +12,7 @@ def generate_qr_codes(frames, *, img_scale: int = 5):
     leading_zeros = len(str(frames))
     format_str = '0' + str(leading_zeros) + 'd'
     for frame in range(frames):
-        # print(frame)
+        print('Frame: ' + str(frame))
         qr = pyqrcode.create(frame + 1)
         img_name = f'Images/QR_Code_Frame_{frame + 1:{format_str}}.png'
         qr.png(img_name, scale=img_scale)
